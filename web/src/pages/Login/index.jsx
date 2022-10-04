e25b9311e16bd946fab371947cbe15c447c1ab41
dopezinho <opugnero@gmail.com>
2022-10-04 15:38:39 -0300

    input + export and import

--- web/src/pages/Login/index.jsx
+++ web/src/pages/Login/index.jsx
@@ -1,11 +1,4 @@
-import { Icon } from './../../components'
-
-const Input = ({ name, label, ...props}) => (
-    <div className="flex flex-col">
-        <label htmlFor={name} className="text-sm font-bold text-gray-500 mb-2">{label}</label>
-        <input {...props} name={name} id={name} className="p-3 border border-gray-700 rounded-xl focus:outline focus:outline-1"/>
-    </div>
-)
+import { Icon, Input } from './../../components'
 
 export const Login = () => {
     return (
