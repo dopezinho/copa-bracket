b5ff4b8c252323dd671971bfdf48f01ba0cbc998
dopezinho <opugnero@gmail.com>
2022-10-04 12:52:18 -0300

    colors + fonts

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,14 +1,9 @@
-const Title = ({children, ...props}) => (
-  <h1 {...props}>{children}</h1>
-)
-
 export function App() {
   return (
-    <div className="bg-red-500">
-      <Title>Olá mundo</Title>
-      <Title>Olá mundo 2</Title>
-      <Title>Olá mundo 3</Title>
+    <div className="h-screen bg-red-700">
+        teste
     </div>
+    
   )
 }
 
