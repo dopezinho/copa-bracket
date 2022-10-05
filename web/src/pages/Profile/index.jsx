202a7a81ec8c3d5ac165b02c54e962a73e17566e
dopezinho <opugnero@gmail.com>
2022-10-05 14:45:02 -0300

    layout done

--- web/src/pages/Profile/index.jsx
+++ web/src/pages/Profile/index.jsx
@@ -1,25 +1,34 @@
 import { Icon, Card } from '~/components'
 
-export const Dashboard = () => (
+export const Profile = () => (
     <>
         <header className="bg-red-500 text-white">
             <div className="container max-w-3xl flex justify-between p-4">
-                <img src="/imgs/logo-fundo-vermelho.svg" classname="w-28 md:w-40"/>
-                <a href="/profile">
-                    <Icon name="profile" className="w-10"/>
-                </a>
+                <img src="/imgs/logo-fundo-vermelho.svg" classname="w-28 md:w-40"/>                
             </div>
         </header>
 
         <main className="space-y-6">
+
             <section id="header" className=" bg-red-500 text-white">
+
                 <div className="container max-w-3xl space-y-2 p-4">
-                    <span>Olá Bruno</span>
-                    <h3 className="text-2xl font-bold">Qual é o seu palpite?</h3>
-                </div>                
+
+                    <a href="/dashboard">
+
+                        <Icon name="back" className="w-10" />
+
+                    </a>
+
+                    <h3 className="text-2xl font-bold">Bruno Bertolini</h3>
+
+                </div>            
+
             </section>
 
             <section id="content" className="container max-w-3xl p-4 space-y-4">
+
+                <h2 className="text-red-500 text-xl font-bold">Seus palpites</h2>
 
                 <div className="flex space-x-4 items-center justify-center">
                     <Icon name="arrowLeft" className="w-6 text-red-500"/>
