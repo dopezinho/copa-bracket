2e63a1787090cedbace457483f46c4f07bf7d0b3
dopezinho <opugnero@gmail.com>
2022-10-06 14:20:16 -0300

    user creating test

--- api/router.js
+++ api/router.js
@@ -1,22 +1,7 @@
 import Router from '@koa/router'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import * as users from './app/users/index.js'
 
 export const router = new Router()
 
-const users = []
-
-router.get('/users', async ctx => {
-    ctx.body = users
-})
-
-router.post('/users', async ctx => {
-    const user = {
-        username: ctx.request.body.username
-    }
-
-    users.push(user)
-
-    ctx.body = user
-})
+router.get('/users', users.list)
+router.post('/users', users.create)
