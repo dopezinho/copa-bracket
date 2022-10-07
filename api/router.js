841ca4f5de35c58856114ed8ad9b945baa66cd19
dopezinho <opugnero@gmail.com>
2022-10-06 21:02:22 -0300

    erro node

--- api/router.js
+++ api/router.js
@@ -1,7 +1,15 @@
 import Router from '@koa/router'
+
 import * as users from './app/users/index.js'
+import * as hunches from './app/hunches/index.js'
+import * as games from './app/games/index.js'
 
 export const router = new Router()
 
 router.get('/users', users.list)
 router.post('/users', users.create)
+
+// router.get('/hunches', hunches.list)
+router.post('/hunches', hunches.create)
+
+router.get('/games', games.list)
