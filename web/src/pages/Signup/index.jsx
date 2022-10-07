db839dfca4d5ac340a1e94eae52e02f90a45e453
dopezinho <opugnero@gmail.com>
2022-10-07 01:57:34 -0300

    cant type on password input

--- web/src/pages/Signup/index.jsx
+++ web/src/pages/Signup/index.jsx
@@ -1,13 +1,40 @@
-import { Icon } from '../../components'
+import axios from 'axios'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
-const Input = ({ name, label, ...props}) => (
-    <div className="flex flex-col">
-        <label htmlFor={name} className="text-sm font-bold text-gray-500 mb-2">{label}</label>
-        <input {...props} name={name} id={name} className="p-3 border border-gray-700 rounded-xl focus:outline focus:outline-1"/>
-    </div>
-)
+import { Icon, Input } from '~/components'
+
+const validationSchema = yup.object().shape({
+    name: yup.string().required('Preencha seu nome'),
+    username: yup.string().required('Preencha seu nome de usuário'),
+    email: yup.string().email('Informe um e-mail válido').required('Informe seu e-mail'),
+    password: yup.string().required('Digite uma senha')
+})
 
 export const Signup = () => {
+    const formik = useFormik({
+        onSubmit: async (values) => {
+
+            const res = await axios({
+                method: 'post',
+                baseURL: 'http://localhost:3000',
+                url: '/users',
+                data: values
+            })
+
+            console.log(res.data)
+            
+        },
+        initialValues: {
+            name: '',
+            username: '',
+            email: '',
+            password: ''
+        },
+        validationSchema
+
+    })
+
     return (
         <div>
             <header className="p-4 border-b border-red-300">
@@ -23,12 +50,16 @@ export const Signup = () => {
                     <h2 className="text-xl font-bold">Crie sua conta</h2>
                 </div>
 
-                <form className="p-4 space-y-6">
+                <form className="p-4 space-y-6" onSubmit={formik.handleSubmit}>
                     <Input
                         type="text"
                         name="name"
-                        label="Seu nome"
+                        label="Seu nome"                        
                         placeholder="Digite seu nome"
+                        error={formik.touched.name && formik.errors.name}
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
 
                     <Input
@@ -36,12 +67,20 @@ export const Signup = () => {
                         name="username"
                         label="Seu nome de usuário"
                         placeholder="Digite um nome de usuário"
+                        error={formik.touched.username && formik.errors.username}
+                        value={formik.values.username}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
                     <Input
                         type="text"
                         name="email"
                         label="Seu e-mail"
                         placeholder="Digite seu e-mail"
+                        error={formik.touched.email && formik.errors.email}
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
 
                     <Input
@@ -49,10 +88,17 @@ export const Signup = () => {
                         name="passowrd"
                         label="Sua senha"
                         placeholder="Digite sua senha"
+                        error={formik.touched.password && formik.errors.password}
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
-                    <a href="/dashboard" className="block w-full text-center text-white bg-red-500 px-6 py-3 rounded-xl">
+                    <button
+                    type="submit" 
+                    className="block w-full text-center text-white bg-red-500 px-6 py-3 rounded-xl disabled:opacity-50" 
+                    disabled={!formik.isValid}>
                         Criar minha Conta
-                    </a>
+                    </button>
                 </form>
             </main>
         </div>
